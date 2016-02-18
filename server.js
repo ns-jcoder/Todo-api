0ab257eb29fa0e07222e947460fe7265dcc259fe
Naeem Shaikh <nayem.ns@gmail.com>
2016-02-18 17:52:38 +0530

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -22,103 +22,76 @@ app.get('/todos', function(req, res) {
 	var query = req.query;
 	var where = {};
 
-	if(query.hasOwnProperty('completed')
-		&& query.completed === 'true'){
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
-	}else if (query.hasOwnProperty('completed')
-		&& query.completed === 'false'){
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
 		where.completed = false;
 	}
 
-	if(query.hasOwnProperty('q')
-		&& query.q.length > 0){
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
 			$like: '%' + query.q + '%'
 		};
 	}
 
-	db.todo.findAll({where: where}).then(function (todos){
-		res.json(todos);
-	},function (e){
-		res.status(500).send();
-	})
-	// var queryParams = req.query;
-	
+	db.todo.findAll({
+			where: where
+		}).then(function(todos) {
+			res.json(todos);
+		}, function(e) {
+			res.status(500).send();
+		})
+		// var queryParams = req.query;
+
 });
 
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 
 
-	 var todoId = parseInt(req.params.id, 10);
-	 db.todo.findById(todoId).then(function (todo) {
-	 	if(!!todo){
-	 		res.json(todo.toJSON());
-	 	}else
-	 	{
-	 		res.status(404).send();
-	 	}
-	 }, function (e){
-	 	res.status(500).send();
-	 });
-	//res.send('Asking for todo with id of '+req.params.id);
-	// var matchedTodo;
-	// var todoId = parseInt(req.params.id, 10);
-	// matchedTodo = _.findWhere(todos, {
-	// 	id: todoId
-	// });
+	var todoId = parseInt(req.params.id, 10);
+	db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function(e) {
+		res.status(500).send();
+	});
 
-	// if (matchedTodo) {
-	// 	res.json(matchedTodo);
-	// } else {
-	// 	res.status(404).send();
-	// }
-	//res.json(todos);
 });
 
 // POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function (todo){
+	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	},function (e){
+	}, function(e) {
 		res.status(400).json(e);
 	});
-/*
 
-
- 
-
-
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-
-	body.description = body.description.trim();
-	//add id field
-	//add the elt to todos array
-	body.id = todoNextId++;
-	todos.push(body);
-
-	//console.log('Description ',body.description);
-	res.json(body);*/
 });
 
 // Delete  /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function() {
+		res.status(500).send();
 	});
-	if (!matchedTodo) {
-		res.status(404).json({
-			"error": "no elt found with passed id "
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
 
 });
 
